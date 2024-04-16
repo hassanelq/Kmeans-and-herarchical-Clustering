5f6384b3e0d357295ec9807ace36879a347d02ec
hassanelq <hassanelqadi2@gmail.com>
2024-04-16 03:28:16 +0100

    Refactor K-means clustering methods and update data input

--- kmeans_Clustering/__init__.py
+++ kmeans_Clustering/__init__.py
@@ -9,33 +9,46 @@ class KmeansClustering:
         self.distance = distance
         self.centroids = None
     
-    def fit(self, Data, max_iter=100):
+    def initialize_centroids(self, Data):
+        """Initialize centroids as random data points from the dataset."""
         self.centroids = np.random.uniform(np.amin(Data, axis=0), np.amax(Data, axis=0), size=(self.k, Data.shape[1]))
+
+    def assign_clusters(self, Data):
+        """Assign data points to the nearest centroid."""
+        clusters = []
+        for data_point in Data:
+            if self.distance == "Euclidean":
+                distances = euclidean_distance(data_point, self.centroids)
+            elif self.distance == "Manhattan":
+                distances = manhattan_distance(data_point, self.centroids)
+            elif self.distance == "Cosine":
+                distances = cosine_distance(data_point, self.centroids)
+            cluster_num = np.argmin(distances)
+            clusters.append(cluster_num)
+        return np.array(clusters)
+
+    def update_centroids(self, Data, labels):
+        """Update centroids to the mean of assigned data points."""
+        new_centroids = np.array([Data[labels == i].mean(axis=0) for i in range(self.k)])
+        return new_centroids
+
+    def fit(self, Data, max_iter=100):
+        """Fit the K-means model to the data."""
+        self.initialize_centroids(Data)
         
         for _ in range(max_iter):
-            y = []
-            for data_point in Data:
-                if self.distance == "Euclidean":
-                    distances = euclidean_distance(data_point, self.centroids)
-                elif self.distance == "Manhattan":
-                    distances = manhattan_distance(data_point, self.centroids)
-                elif self.distance == "Cosine":
-                    distances = cosine_distance(data_point, self.centroids)
-                cluster_num = np.argmin(distances)
-                y.append(cluster_num)
-            y = np.array(y)
-
-            new_centroids = np.array([Data[y == i].mean(axis=0) for i in range(self.k)])
-
-            # Check if centroids have converged
+            labels = self.assign_clusters(Data)
+            new_centroids = self.update_centroids(Data, labels)
+            
             if np.all(np.linalg.norm(self.centroids - new_centroids, axis=1) < 0.0001):
                 break
             else:
                 self.centroids = new_centroids
         
-        return y
+        return labels
     
     def plot_clusters(self, Data, labels):
+        """Visualize the clusters and centroids."""
         colors = ["#4C72B0", "#DD8452", "#55A868", "#C44E52", "#8172B3",
                   "#937860", "#DA8BC3", "#8C8C8C", "#CCB974", "#64B5CD"]
         fig, ax = plt.subplots()
@@ -48,33 +61,6 @@ class KmeansClustering:
         plt.xlabel('Feature 1')
         plt.ylabel('Feature 2')
         plt.legend()
+        plt.show()
 
         return fig
-    
-    def within_cluster_sum_of_squares(self):
-        wcss = 0
-        for i, cluster in enumerate(self.clusters):
-            wcss += np.sum((cluster - self.centroids[i]) ** 2)
-        return wcss
-    
-    @staticmethod
-    def calculate_wcss(data, k_range):
-        # Adjusted to use KmeansClustering
-        wcss_values = []
-        for k in k_range:
-            kmeans = KmeansClustering(k=k)
-            kmeans.fit(data)
-            wcss = kmeans.within_cluster_sum_of_squares()
-            wcss_values.append(wcss)
-        return wcss_values
-
-    @staticmethod
-    def plot_elbow_curve(k_range, wcss_values):
-        fig = plt.figure(figsize=(8, 5))
-        plt.plot(k_range, wcss_values, 'bo-', markersize=8, lw=2)
-        plt.title('Elbow Method For Optimal k')
-        plt.xlabel('Number of clusters (k)')
-        plt.ylabel('Within-Cluster Sum of Squares (WCSS)')
-        plt.grid(True)
-        return fig
-        
